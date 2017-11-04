cb432d191bb731ff9e4987a85798766138f198cb
S1awek <elvizky@gmail.com>
2017-11-04 20:55:23 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+/**
+ * MyClass Class Doc Comment
+ * 
+ * PHP version 7.1
+ *
+ * @category Class
+ * @package  MyClass
+ * @author   Slawek <slawek@some.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://www.hashbangcode.com/
+ */
+
+?>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Random nick generator</title>
+  <meta name="description" content="Generate cool random nickname">
+  <meta name="author" content="S1awek">
+
+  <link rel="stylesheet" href="css/style.css?v=1.0">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<?php echo('<h1 class="test">TEST1</h1>'); ?>
+<script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
+
+
+    
